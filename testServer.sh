2a4c5810ca43a648595db05b8c245cc40b7456e6
csamuro <3danger@mail,ru>
2022-04-23 19:41:51 +0300

    added script testServer.sh

---
+++ testServer.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+
+go build main.go
+
+gnome-terminal -x ./main
+sleep 1
+gnome-terminal -x sh -c {
+  cd Publisher &&
+  go build publisher.go &&
+  cd .. &&
+  ./Publisher/publisher -j ./Publisher/json -ms 300;
+}
+
